dd33ae6a149152a901bcbd36beef1db8098bb460
devAlq <khalidalqdev@gmail.com>
2024-08-29 15:17:03 +0300

    taskDone

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,35 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Main {
+    public static void main(String[] args) throws Exception
+    {
+        Path path1 = Paths.get("C:\\Users\\khali\\Downloads\\chalenge\\recovery-project\\src\\first.txt");
+        Path path2 = Paths.get("C:\\Users\\khali\\Downloads\\chalenge\\recovery-project\\src\\second.txt");
+        // Read files into byte arrays
+        byte[] data1 = Files.readAllBytes(path1);
+        byte[] data2 = Files.readAllBytes(path2);
+
+        FileWriter myWriter = new FileWriter("C:\\\\Users\\\\khali\\\\Downloads\\\\chalenge\\\\recovery-project\\\\src\\\\result.out");
+       for (int i=0;i<data1.length;i++) {
+        myWriter.write(data1[i] ^ data2[i]);}
+        myWriter.close();
+        Path path3 = Paths.get("C:\\Users\\khali\\Downloads\\chalenge\\recovery-project\\src\\result.out");
+
+        byte []data3 = Files.readAllBytes(path3);
+        
+        for (int i=0;i<data1.length;i++) {
+            data3[i]=(byte) (data1[i] ^ data3[i]);
+        }
+
+        for (int i=0;i<data1.length;i++) {
+            System.out.print(data3[i] +" ");
+        }System.out.println();
+        for (int i=0;i<data1.length;i++) {
+            System.out.print(data2[i] +" ");
+        }
     
-    public static void main(String[] args) {
-        
-        // Your code here
-        
-    }
-}
+    }}
+    
